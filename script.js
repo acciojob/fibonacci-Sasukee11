6cb603a79e849fe5e7fff6f6306a7d1ac7e04725
Sasuke <118628015+Sasukee11@users.noreply.github.com>
2023-07-12 16:15:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function fibonacci(num) {
 // your code here
+	let n1 = 0, n2 = 1, nextTerm;
+	const arr = [];
 	for (let i = 1; i <= number; i++) {
-    console.log(n1);
+	arr.push(n1);
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
